751847c3565dfbf718e39309552509f7fe5fc4e3
unknown <abusayed10809@gmail.com>
2023-04-29 23:12:48 +0600

    wip, user stats

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
 import "./widgetLg.css";
+import { useState } from "react";
+import { userRequest } from "../../requestMethods";
+import { format } from "timeago.js";
 
 export default function WidgetLg() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await userRequest.get("/orders");
+        setOrders(res.data);
+      } catch (error) {
+        console.log("get order error ->>> " + error);
+      }
+    };
+
+    getOrders();
+  }, []);
+
   const Button = ({ type }) => {
     return <button className={"widgetLgButton " + type}>{type}</button>;
   };
@@ -17,69 +36,26 @@ export default function WidgetLg() {
             <th className="widgetLgTh">Status</th>
           </tr>
 
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Approved" />
-            </td>
-          </tr>
-
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Declined" />
-            </td>
-          </tr>
-
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Pending" />
-            </td>
-          </tr>
-
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Approved" />
-            </td>
-          </tr>
+          {orders &&
+            orders.map((order) => (
+              <tr className="widgetLgTr">
+                <td className="widgetLgUser">
+                  <img
+                    src={
+                      "https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    }
+                    alt=""
+                    className="widgetLgImg"
+                  />
+                  <span className="widgetLgName">{order.userId}</span>
+                </td>
+                <td className="widgetLgDate">{format(order.createdAt)}</td>
+                <td className="widgetLgAmount">${order.amount}</td>
+                <td className="widgetLgStatus">
+                  <Button type={order.status} />
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,15 +4,58 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { useState } from "react";
+import { useMemo } from "react";
 
 export default function Home() {
+  const [userStats, setUserStats] = useState([]);
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/user/stats");
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "Active User": item.total },
+          ])
+        );
+      } catch {}
+    };
+
+    getStats();
+  }, [MONTHS]);
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
